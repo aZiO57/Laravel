1aa3914532a110fe48f9db948f16e9f7c97bb876
Deividask123 <deividask123@gmail.com>
2022-04-25 18:03:43 +0300

    Ad creation added

--- skelbimai/routes/web.php
+++ skelbimai/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('test', 'App\Hettp\Controllers\TestController.php');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/ad', 'App\Http\Controllers\AdController');
